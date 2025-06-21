8d786f344af72bc8e806f007b648ba22b4240a08
Saloni Gowadia <salonigowadia@gmail.com>
2025-06-21 13:32:36 +0530

    testing

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,18 @@ dotenv.config();
 import connectDB from "@src/db/index";
 import app from "@src/app";
 
+console.log("connectin1...");
+app.get("/test", (req, res) => {
+  res.send("test is running");
+});
 connectDB()
   .then(() => {
+    app.get("/", (req, res) => {
+      res.send("api is running");
+    });
+
     app.listen(process.env.PORT || 8000, () => {
       console.log("Server is running at port:", process.env.PORT);
-      app.get("/", (req, res) => {
-        res.send("api is running");
-      });
     });
   })
   .catch((error) => {
