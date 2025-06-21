4799b343a3bb001b4c395d93aba463f36a77d06b
Saloni Gowadia <salonigowadia@gmail.com>
2025-06-21 12:43:55 +0530

    endpoind added

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,9 @@ import cartRoute from "@src/api/routes/cart.route";
 // import paymentRoute from "@src/api/routes/payment.routes";
 // import orderRoute from "@src/api/routes/order.route";
 //routes
-
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
 app.use("/product", productRoute);
 app.use("/category", categoryRoute);
 app.use("/type", typeRoute);
