3c8cb2ee91584103cc108a9668c5c5d8daa922b0
Saloni Gowadia <salonigowadia@gmail.com>
2025-06-23 23:08:14 +0530

    chng 9

--- index.ts
+++ index.ts
@@ -1,7 +1,26 @@
 // import express from "express";
-import app from "./src/app";
-// const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+// import { errorHandler } from "@src/utils/errorHandler";
+// import app from "./src/app";
+const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, //"http://localhost:5173"
+    credentials: true, /// explore
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "16kb" })); // accept json
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
 // Index route
 app.get("/", (req, res) => {
   res.send("Welcome to the Home Page!");
