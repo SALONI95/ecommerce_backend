cca57b7f280e2a278255d4784676ea1c273eeab4
Saloni Gowadia <salonigowadia@gmail.com>
2025-06-23 23:11:14 +0530

    chng 9

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-// import { errorHandler } from "@src/utils/errorHandler";
+import { errorHandler } from "@src/utils/errorHandler";
 // import app from "./src/app";
 const app = express();
 
@@ -34,5 +34,7 @@ app.get("/health", (req, res) => {
     timestamp: new Date(),
   });
 });
+//Throw Error
+app.use(errorHandler);
 
 export default app;
