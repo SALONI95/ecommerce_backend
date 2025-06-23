5b2be0fb3a838736ea75b6b6be1695e016d4f9b0
Saloni Gowadia <salonigowadia@gmail.com>
2025-06-23 22:20:25 +0530

    change 4

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+
+// Index route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Home Page!");
+});
+
+// Health check route
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: new Date(),
+  });
+});
+
+export default app;
