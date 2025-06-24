513e115aa96f97d909953e10e40aac7a768f7caa
Saloni Gowadia <salonigowadia@gmail.com>
2025-06-24 13:25:52 +0530

    changes vercl

---
+++ api/index.ts
@@ -0,0 +1,40 @@
+// import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { errorHandler } from "@src/utils/errorHandler";
+// import app from "./src/app";
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, //"http://localhost:5173"
+    credentials: true, /// explore
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "16kb" })); // accept json
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+// Index route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Home Page!");
+});
+
+// Health check route
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: new Date(),
+  });
+});
+//Throw Error
+app.use(errorHandler);
+
+export default app;
